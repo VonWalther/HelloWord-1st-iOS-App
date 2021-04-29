f59ca4bd3b8df79b72d3fd60554b2c145dff9e4a
VonWalther <46531452+VonWalther@users.noreply.github.com>
2021-04-29 13:39:57 -0500

    Initial Commit

---
+++ HelloWord/HelloWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWordApp.swift
+//  HelloWord
+//
+//  Created by Carl Walther on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
