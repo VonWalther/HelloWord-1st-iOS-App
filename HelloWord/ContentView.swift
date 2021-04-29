acc5096c07b8e3a01125fa74c6e0d6a84d9fe8cc
VonWalther <46531452+VonWalther@users.noreply.github.com>
2021-04-29 14:41:20 -0500

    Hello Word, this is a brave new world that I am entering.

--- HelloWord/ContentView.swift
+++ HelloWord/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, how are you?")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .foregroundColor(Color.red)
+            .padding([.leading, .bottom], 17.0)
+            
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+                
+        }
+            
+            
+            
     }
 }
